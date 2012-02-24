46a9a737fa446ebb8264f7c141ee172d95e22c66
Jeff Kunkle <jeff.kunkle@nearinfinity.com>
2012-02-24 14:27:40 -0500

    Add testing for object extensions

---
+++ test/extensions/object.js
@@ -0,0 +1,114 @@
+var expect = require('chai').expect;
+var objext = require('../../lib/extensions/object');
+
+describe('object', function() {
+  describe('#isString', function() {
+    it('should return false when passed a null or undefined value', function(done) {
+      expect(objext.isString(null)).to.be.false;
+      expect(objext.isString(undefined)).to.be.false;
+      done();
+    })
+
+    it('should return true when passed a string', function(done) {
+      expect(objext.isString('')).to.be.true;
+      expect(objext.isString.call('')).to.be.true;
+      done();
+    })
+
+    it('should return false when passed anything other than a string', function(done) {
+      expect(objext.isString(4)).to.be.false;
+      expect(objext.isString([])).to.be.false;
+      expect(objext.isString(function() {})).to.be.false;
+      expect(objext.isString({})).to.be.false;
+      done();
+    })
+  })
+
+  describe('#isFunction', function() {
+    it('should return false when passed a null or undefined value', function(done) {
+      expect(objext.isFunction(null)).to.be.false;
+      expect(objext.isFunction(undefined)).to.be.false;
+      done();
+    })
+
+    it('should return true when passed a function', function(done) {
+      expect(objext.isFunction(function() {})).to.be.true;
+      expect(objext.isFunction.call(function() {})).to.be.true;
+      done();
+    })
+
+    it('should return false when passed anything other than a function', function(done) {
+      expect(objext.isFunction(4)).to.be.false;
+      expect(objext.isFunction([])).to.be.false;
+      expect(objext.isFunction({})).to.be.false;
+      expect(objext.isFunction('')).to.be.false;
+      done();
+    })
+  })
+
+  describe('#isArray', function() {
+    it('should return false when passed a null or undefined value', function(done) {
+      expect(objext.isArray(null)).to.be.false;
+      expect(objext.isArray(undefined)).to.be.false;
+      done();
+    })
+
+    it('should return true when passed an array', function(done) {
+      expect(objext.isArray([])).to.be.true;
+      expect(objext.isArray.call([])).to.be.true;
+      done();
+    })
+
+    it('should return false when passed anything other than an array', function(done) {
+      expect(objext.isArray(4)).to.be.false;
+      expect(objext.isArray(function() {})).to.be.false;
+      expect(objext.isArray({})).to.be.false;
+      expect(objext.isArray('')).to.be.false;
+      done();
+    })
+  })
+
+  describe('#isNumber', function() {
+    it('should return false when passed a null or undefined value', function(done) {
+      expect(objext.isNumber(null)).to.be.false;
+      expect(objext.isNumber(undefined)).to.be.false;
+      done();
+    })
+
+    it('should return true when passed a number', function(done) {
+      expect(objext.isNumber(7)).to.be.true;
+      expect(objext.isNumber.call(7)).to.be.true;
+      done();
+    })
+
+    it('should return false when passed anything other than a number', function(done) {
+      expect(objext.isNumber(function() {})).to.be.false;
+      expect(objext.isNumber([])).to.be.false;
+      expect(objext.isNumber({})).to.be.false;
+      expect(objext.isNumber('')).to.be.false;
+      done();
+    })
+  })
+
+  describe('#isObject', function() {
+    it('should return false when passed a null or undefined value', function(done) {
+      expect(objext.isObject(null)).to.be.false;
+      expect(objext.isObject(undefined)).to.be.false;
+      done();
+    })
+
+    it('should return true when passed an object', function(done) {
+      expect(objext.isObject({})).to.be.true;
+      expect(objext.isObject.call({})).to.be.true;
+      done();
+    })
+
+    it('should return false when passed anything other than an object', function(done) {
+      expect(objext.isObject(function() {})).to.be.false;
+      expect(objext.isObject([])).to.be.false;
+      expect(objext.isObject(4)).to.be.false;
+      expect(objext.isObject('')).to.be.false;
+      done();
+    })
+  })
+})
