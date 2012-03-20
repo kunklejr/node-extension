749f923d1ee0b12b2b5901e851ea7b80efb85731
Joe Ferner <joe@fernsroth.com>
2012-03-20 10:54:14 -0400

    added groupBy

--- test/array.js
+++ test/array.js
@@ -35,4 +35,31 @@ describe('array', function() {
       done();
     })
   })
+
+  describe('#groupBy', function() {
+    it('should group by name', function(done) {
+      var result = arrext.groupBy([
+        { name: 'bin1', value: 1 },
+        { name: 'bin2', value: 2 },
+        { name: 'bin1', value: 3 },
+        { name: 'bin3', value: 4 },
+        { name: 'bin2', value: 5 }
+      ], function(item) { return item.name });
+
+      expect(result).to.eql({
+        'bin1': [
+          { name: 'bin1', value: 1 },
+          { name: 'bin1', value: 3 }
+        ],
+        'bin2': [
+          { name: 'bin2', value: 2 },
+          { name: 'bin2', value: 5 }
+        ],
+        'bin3': [
+          { name: 'bin3', value: 4 }
+        ]
+      });
+      done();
+    })
+  })
 })
