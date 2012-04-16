db3d698c273a851644b7a04d9ddb50f7c995299b
Joe Ferner <joe@fernsroth.com>
2012-04-16 10:21:05 -0400

    added sortCaseInsensitive

--- test/array.js
+++ test/array.js
@@ -7,34 +7,42 @@ describe('array', function() {
       expect(arrext.contains([], 1)).to.be.false;
       expect(arrext.contains.call([], 1)).to.be.false;
       done();
-    })
+    });
 
     it('should return false when the array does not contain the value', function(done) {
       expect(arrext.contains([1, 2], 3)).to.be.false;
       expect(arrext.contains.call([1, 2], 3)).to.be.false;
       done();
-    })
+    });
 
     it('should return true when the array contains the value', function(done) {
       expect(arrext.contains([1, 2], 2)).to.be.true;
       expect(arrext.contains.call([1, 2], 2)).to.be.true;
       done();
     })
-  })
+  });
 
   describe('#isEmpty', function() {
     it('should return true when empty', function(done) {
       expect(arrext.isEmpty([])).to.be.true;
       expect(arrext.isEmpty.call([])).to.be.true;
       done();
-    })
+    });
 
     it('should return false when not empty', function(done) {
       expect(arrext.isEmpty([1])).to.be.false;
       expect(arrext.isEmpty.call([1])).to.be.false;
       done();
-    })
-  })
+    });
+  });
+
+  describe('#sortCaseInsensitive', function() {
+    it('should return true when empty', function(done) {
+      var result = arrext.sortCaseInsensitive(['az', 'Ab', 'aa', 1]);
+      expect(result).to.eql([1, 'aa', 'Ab', 'az']);
+      done();
+    });
+  });
 
   describe('#groupBy', function() {
     it('should group by name', function(done) {
@@ -60,7 +68,7 @@ describe('array', function() {
         ]
       });
       done();
-    })
+    });
 
     it('should group by name (this)', function(done) {
       var result = arrext.groupBy.call([
@@ -85,6 +93,6 @@ describe('array', function() {
         ]
       });
       done();
-    })
-  })
-})
+    });
+  });
+});
